81215e6f827e11fd5085eca0971d6cd132df2363
wangyongfeng <1084415961@qq.com>
2022-10-05 12:49:00 +0800

    feat: 创建主应用

--- src/index.js
+++ src/index.js
@@ -3,7 +3,23 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { registerMicroApps, start } from 'qiankun';
 
+registerMicroApps([
+  {
+    name: 'data-admin', // app name registered
+    entry: '//localhost:8000',
+    container: '#root',
+    activeRule: '/yourActiveRule',
+  }
+]);
+
+start({
+  sandbox: {
+    strictStyleIsolation: true,
+    experimentalStyleIsolation: true
+  }
+});
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
