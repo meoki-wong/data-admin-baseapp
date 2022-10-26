3c8d26c22fec5c5498407932952f42d50fef4bf4
wangyongfeng <1084415961@qq.com>
2022-10-26 18:06:53 +0800

    feat: 部署qiankun

--- src/index.js
+++ src/index.js
@@ -1,46 +1,59 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { registerMicroApps, start, setDefaultMountApp } from 'qiankun';
-import apps from './apps'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { registerMicroApps, start, setDefaultMountApp } from "qiankun";
+import apps from "./apps";
+import { BrowserRouter } from "react-router-dom";
 
 registerMicroApps(apps, {
-  beforeLoad: app => {
-    console.log('before load app.name=====>>>>>', app.name)
-  },
-  beforeMount: [
-    app => {
-      console.log('[LifeCycle] before mount %c%s', 'color: green;', app.name)
-    }
-  ],
-  afterMount: [
-    app => {
-      console.log('[LifeCycle] after mount %c%s', 'color: green;', app.name)
-    }
-  ],
-  afterUnmount: [
-    app => {
-      console.log('[LifeCycle] after unmount %c%s', 'color: green;', app.name)
-    }
-  ]
-})
+	beforeLoad: (app) => {
+		console.log("before load app.name=====>>>>>", app.name);
+	},
+	beforeMount: [
+		(app) => {
+			console.log(
+				"[LifeCycle] before mount %c%s",
+				"color: green;",
+				app.name
+			);
+		},
+	],
+	afterMount: [
+		(app) => {
+			console.log(
+				"[LifeCycle] after mount %c%s",
+				"color: green;",
+				app.name
+			);
+		},
+	],
+	afterUnmount: [
+		(app) => {
+			console.log(
+				"[LifeCycle] after unmount %c%s",
+				"color: green;",
+				app.name
+			);
+		},
+	],
+});
 
 start({
-  sandbox: {
-    // strictStyleIsolation: true,
-    // experimentalStyleIsolation: true
-    // prefetch: "all"
-  }
+	sandbox: {
+		// strictStyleIsolation: true,
+		// experimentalStyleIsolation: true
+		// prefetch: "all"
+	},
 });
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+		<React.StrictMode>
+			<App />
+		</React.StrictMode>
 );
-setDefaultMountApp('/blog') // 默认进入微应用
+setDefaultMountApp("/blog"); // 默认进入微应用
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
